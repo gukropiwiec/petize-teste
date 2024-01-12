7b20fa37c60f73bdc94eab90d8d3d595963a59cd
Gustavo Kropiwiec <gustavosk17@hotmail.com>
2024-01-12 10:21:39 -0300

    deploy

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { DetalhesComponent } from './components/detalhes/detalhes.component';
-
-export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: ':id', component: DetalhesComponent }
-];
